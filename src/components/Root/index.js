f2552be913e7c887e7d019c6b8a2ec9ba5d09912
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-09-24 21:38:27 +0300

    Add "Root" component

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,24 +1,13 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
-import { hot } from 'react-hot-loader'
-//
+import {Router} from 'react-static'
+import {hot} from 'react-hot-loader'
 import Routes from 'react-static-routes'
-
 import './index.scss'
 
 const App = () => (
     <Router>
-        <div>
-            <nav>
-                <Link exact to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/blog">Blog</Link>
-            </nav>
-            <div className="content">
-                <Routes />
-            </div>
-        </div>
+        <Routes />
     </Router>
-)
+);
 
-export default hot(module)(App)
+export default hot(module)(App);
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {withSiteData} from 'react-static'
+
+/**
+ * Renders Home component
+ * @returns {JSX}
+ */
+const Home = () => (
+    <div>Home</div>
+);
+
+export default withSiteData(Home);
+
