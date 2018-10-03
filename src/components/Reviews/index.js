332c23c25ce5867c0d5597f99aed4b96f98682e6
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-04 00:14:37 +0300

    Add "Pagination" component

--- src/components/Reviews/index.js
+++ src/components/Reviews/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Slider from 'react-slick';
 import Review from '../Review';
 import reviews from '../../data/reviews';
+import Pagination from '../Pagination';
 import './index.scss';
 
 const sliderSettings = {
@@ -21,7 +22,13 @@ class Reviews extends Component {
     constructor(props) {
         super(props);
 
-        this.slider = React.createRef();
+        this.onNext = this.onNext.bind(this);
+        this.onPrev = this.onPrev.bind(this);
+        this.onChange = this.onChange.bind(this);
+
+        this.state = {
+            currentSlide: 1
+        };
     }
 
     /**
@@ -39,21 +46,43 @@ class Reviews extends Component {
     }
 
     /**
+     * Handles change event
+     * @param {Number} current
+     * @param {Number} next
+     */
+    onChange(current, next = 0) {
+        const currentSlide = next + 1;
+
+        this.setState({currentSlide});
+    }
+
+    /**
      * Renders component
      * @returns {JSX}
      */
     render() {
+        const {currentSlide = 0} = this.state;
+
         return (
             <section className="Reviews">
                 <div className="Reviews__wrapper">
                     <div className="Reviews__content">
-                        <Slider {...sliderSettings} ref={this.slider}>
+                        <Slider
+                            {...sliderSettings}
+                            beforeChange={this.onChange}
+                            ref={(slider) => (this.slider = slider)}>
                             {
                                 reviews.map((review, idx) => <div key={idx}><Review {...review} /></div>)
                             }
                         </Slider>
 
-                        <div className="Reviews__nav" />
+                        <div className="Reviews__nav">
+                            <Pagination
+                                current={currentSlide}
+                                total={reviews.length}
+                                onPrev={this.onPrev}
+                                onNext={this.onNext} />
+                        </div>
                     </div>
                 </div>
             </section>
