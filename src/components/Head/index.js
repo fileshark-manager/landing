29aa801f0b26b7c4a4b02f1b327ee3ccc2276c37
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-12 20:53:54 +0300

    Add "Head" component

---
+++ src/components/Head/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Head} from 'react-static';
+import faviconApple from '../../assets/images/favicon/apple-touch-icon.png';
+import faviconBig from '../../assets/images/favicon/favicon-32x32.png';
+import faviconSmall from '../../assets/images/favicon/favicon-16x16.png';
+
+import faviconSafari from '../../assets/images/favicon/safari-pinned-tab.svg';
+import faviconIco from '../../assets/images/favicon/favicon.ico';
+
+/**
+ * Renders HeadContainer component
+ * @param {Object}
+ * @returns {JSX}
+ */
+const HeadContainer = ({
+    title,
+    metaDescription,
+    metaKeywords,
+}) => (
+    <Head>
+        <meta name="Description" content={metaDescription} />
+        <meta name="Keywords" content={metaKeywords} />
+
+        <title>{title}</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href={faviconApple} />
+        <link rel="icon" type="image/png" sizes="32x32" href={faviconBig} />
+        <link rel="icon" type="image/png" sizes="16x16" href={faviconSmall} />
+        <link rel="manifest" href="/static/images/favicon/site.webmanifest" />
+        <link rel="mask-icon" href={faviconSafari} color="#5bbad5" />
+        <link rel="shortcut icon" href={faviconIco} />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/static/images/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#FFFFFF" />
+    </Head>
+);
+
+HeadContainer.propTypes = {
+    title: PropTypes.string,
+    metaDescription: PropTypes.string,
+    metaKeywords: PropTypes.string
+};
+
+export default HeadContainer;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from '../Head';
 import Header from '../Header';
 import Main from '../Main';
 import Footer from '../Footer';
@@ -10,9 +11,17 @@ import Footer from '../Footer';
  * @returns {JSX}
  */
 const Layout = ({
+    title,
+    metaDescription,
+    metaKeywords,
     children
 }) => (
     <div className="Layout">
+        <Head
+            title={title}
+            metaDescription={metaDescription}
+            metaKeywords={metaKeywords} />
+
         <div className="Layout__header">
             <Header />
         </div>
@@ -30,7 +39,16 @@ const Layout = ({
 );
 
 Layout.propTypes = {
+    title: PropTypes.string,
+    metaDescription: PropTypes.string,
+    metaKeywords: PropTypes.string,
     children: PropTypes.node
+};
+
+Layout.defaultProps = {
+    title: 'File.Shark Media manager - Custom interface solution for file management in your content management system.',
+    metaDescription: 'File.Shark Media manager - Custom interface solution for file management in your content management system.',
+    metaKeywords: 'File.Shark, media manage, interface solution, upload files, create folders, make descriptions, filters, cms, content management system, javascript, buy'
 };
 
 export default Layout;
