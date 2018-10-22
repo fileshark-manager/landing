6f5416da007377bb044527e3d5e04b3c31322d9c
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-22 23:59:24 +0300

    Add "Server" component

--- src/components/Documentation/modules/Server/index.js
+++ src/components/Documentation/modules/Server/index.js
@@ -18,7 +18,11 @@ const Server = () => (
 
         <p>This functionality allows you to create, edit and delete folders. If you are sure that you do not need this functionality, you can skip this part and then you will have one folder by default.</p>
 
-        <h4>POST /yourFolderEndpoint - create a new folder</h4>
+        <h4>
+            <ul>
+                <li>POST /yourFolderEndpoint - create a new folder</li>
+            </ul>
+        </h4>
 
         <p>Request parameters:</p>
 
@@ -99,6 +103,126 @@ const Server = () => (
                 </td>
             </tr>
         </DocumentationTable>
+
+        <br />
+        <br />
+
+        <h4>
+            <ul>
+                <li>PUT /yourFolderEndpoint - edit folder</li>
+            </ul>
+        </h4>
+
+        <p>Request parameters:</p>
+
+        <DocumentationTable>
+            <tr>
+                <th>Param</th>
+                <th>Type and Description</th>
+            </tr>
+
+            <tr>
+                <td>title</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: folder name</p>
+                </td>
+            </tr>
+
+            <tr>
+                <td>description</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: folder description</p>
+                </td>
+            </tr>
+        </DocumentationTable>
+
+        <p>Expected response:</p>
+
+        <DocumentationCode>
+            {
+                `
+    {
+        created: "15.10.2018"
+        description: ""
+        id: "dfd8c3e4-45b3-4278-acba-467b42dae5f9"
+        title: "Nature"
+        total: "0"
+    }
+                `
+            }
+        </DocumentationCode>
+
+        <DocumentationTable>
+            <tr>
+                <th>Param</th>
+                <th>Type and Description</th>
+            </tr>
+
+            <tr>
+                <td>created</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: creation date</p>
+                </td>
+            </tr>
+
+            <tr>
+                <td>description</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: folder description</p>
+                </td>
+            </tr>
+
+            <tr>
+                <td>id</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: edited folder id</p>
+                </td>
+            </tr>
+
+            <tr>
+                <td>title</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: edited folder title</p>
+                </td>
+            </tr>
+
+            <tr>
+                <td>total</td>
+                <td>
+                    <p>Type: String</p>
+                    <p>Description: number of files in folder</p>
+                </td>
+            </tr>
+        </DocumentationTable>
+
+        <br />
+        <br />
+
+        <h4>
+            <ul>
+                <li>DELETE /yourFolderEndpoint/idOfDeletedFolder - delete folder</li>
+            </ul>
+        </h4>
+
+        <p>No request parameters.</p>
+
+        <p>Expected response:</p>
+
+        <DocumentationCode>
+            {'dfd8c3e4-45b3-4278-acba-467b42dae5f9'}
+        </DocumentationCode>
+
+        <DocumentationInfo type="String" description="id of deleted folder" />
+
+        <h3>Files</h3>
+
+        <p>This functionality allows you to upload, edit and delete files.</p>
     </DocumentationSection>
 );
 
