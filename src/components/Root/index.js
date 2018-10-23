00d2207e1629098f80ecbb39ef3494f5268a6a23
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-24 00:25:29 +0300

    Update Root

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Router} from 'react-static';
 import {hot} from 'react-hot-loader';
 import Routes from 'react-static-routes';
-import '../../assets/fonts/montserrat';
 import './index.scss';
 
 const App = () => (
