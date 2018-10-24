eacbddce668e4421d1c6eb91b63ee346f1113cd2
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-24 23:00:15 +0300

    Update "App" component

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Router} from 'react-static';
 import {hot} from 'react-hot-loader';
 import Routes from 'react-static-routes';
+import ReactGA from 'react-ga';
 import './index.scss';
 
-const App = () => (
-    <Router>
-        <Routes />
-    </Router>
-);
+class App extends Component {
+    componentWillMount() {
+        ReactGA.initialize('UA-128053424-1');
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes />
+            </Router>
+        );
+    }
+}
 
 export default hot(module)(App);
