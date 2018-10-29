4439cca9c2e83cdd38f1588f5d74b11d1c8724de
Дмитрий Малофеев <dim2k2006@yandex.ru>
2018-10-29 19:49:24 +0300

    Add google verification file

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,5 +4,8 @@ gulp.task('copy', () => {
     gulp.src('./src/assets/images/favicon/**')
         .pipe(gulp.dest('./dist/static/images/favicon'));
 
+    gulp.src('./google5af196874a5d7276.html')
+        .pipe(gulp.dest('./dist'));
+
     return true;
 });
