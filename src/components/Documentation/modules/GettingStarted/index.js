c9832f434772a048f5a665f9bfb998d79bab53d1
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-03-12 22:05:10 +0300

    Update documentation

--- src/components/Documentation/modules/GettingStarted/index.js
+++ src/components/Documentation/modules/GettingStarted/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-static';
 import DocumentationSection from '../../../DocumentationSection';
 
 /**
@@ -13,8 +14,8 @@ const GettingStarted = () => (
 
         <ul>
             <li>download archive with files (in this archive you will find <i>index.html</i> file with example of launching media manager, <i>File.Shark Media manager - Documentation.pdf</i> file with documentation, js folder with <i>main-[hash].js</i> file)</li>
-            <li>include all required assets</li>
-            <li>initialize «File.Shark» media manager</li>
+            <li>include <i>main-[hash].js</i> file to your page</li>
+            <li>initialize «File.Shark» media manager (check <Link to="#usage">usage</Link> section for more details)</li>
             <li>configure the server to work with media manager</li>
         </ul>
     </DocumentationSection>
