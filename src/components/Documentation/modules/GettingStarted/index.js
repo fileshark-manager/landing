9a971dbe9dec9a42592e94c74403f822844b0ce4
Дмитрий Малофеев <dim2k2006@yandex.ru>
2019-03-12 22:01:09 +0300

    Update documentation

--- src/components/Documentation/modules/GettingStarted/index.js
+++ src/components/Documentation/modules/GettingStarted/index.js
@@ -12,7 +12,7 @@ const GettingStarted = () => (
         <p>To start using «File.Shark» media manager it is necessary to do the next things:</p>
 
         <ul>
-            <li>download archive with files</li>
+            <li>download archive with files (in this archive you will find <i>index.html</i> file with example of launching media manager, <i>File.Shark Media manager - Documentation.pdf</i> file with documentation, js folder with <i>main-[hash].js</i> file)</li>
             <li>include all required assets</li>
             <li>initialize «File.Shark» media manager</li>
             <li>configure the server to work with media manager</li>
